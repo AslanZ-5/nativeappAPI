0e93e01482a7f73fc8ce7c5dd18d71342c52a37c
Aslan Zur <asl.zurabov@gmail.com>
2023-08-26 19:15:47 +0300

    creating api enpoints

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from .models import Article
+from .serializers import ArticleSerializer
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+@csrf_exempt
+def article_list(request):
+    if request.method == "GET":
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)

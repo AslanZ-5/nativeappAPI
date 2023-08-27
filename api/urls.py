2bd380fb183c61ea2107988ba2ec1d7268c766f1
Aslan Zur <asl.zurabov@gmail.com>
2023-08-27 13:32:01 +0300

    generic viewsets

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from .views import article_list, article_details
+from django.urls import path, include
+from rest_framework import routers
+from .views import ArticleViewSet
+# from .views import article_list, article_details
+router = routers.DefaultRouter()
+router.register('articles', ArticleViewSet, basename='article')
 urlpatterns = [
-    path('articles/', article_list),
-    path('articles/<int:pk>/', article_details),
+    path('', include(router.urls))
+    # path('articles/', article_list),
+    # path('articles/<int:pk>/', article_details),
 ]
--- api/views.py
+++ api/views.py
@@ -1,44 +1,59 @@
-from django.shortcuts import HttpResponse
 from .models import Article
 from .serializers import ArticleSerializer
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import (
+    ListModelMixin,
+    CreateModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin
+)
 
-@csrf_exempt
-def article_list(request):
-    if request.method == "GET":
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
 
-@csrf_exempt
-def article_details(request,pk):
-    try:
-        article = Article.objects.get(pk=pk)
-    
-    except Article.DoesNotExist:
-        return HttpResponse(status=404)
 
-    if request.method == "GET":
-        serializer = ArticleSerializer(article)
-        return JsonResponse(serializer.data)
+
+class ArticleViewSet(GenericViewSet, ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin) :
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
     
-    elif request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializer(article, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+
+
+
+
+
+
+# @api_view(["GET","POST"])
+# def article_list(request):
+#     if request.method == "GET":
+#         articles = Article.objects.all()
+#         serializer = ArticleSerializer(articles, many=True)
+#         return Response(serializer.data)
+#     elif request.method == "POST":
+#         serializer = ArticleSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(["GET","PUT","DELETE"])
+# def article_details(request,pk):
+#     try:
+#         article = Article.objects.get(pk=pk)
     
-    elif request.method == "DELETE":
-        article.delete()
-        return HttpResponse(status=204)
+#     except Article.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == "GET":
+#         serializer = ArticleSerializer(article)
+#         return Response(serializer.data)
+    
+#     elif request.method == "PUT":
+#         serializer = ArticleSerializer(article, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#     elif request.method == "DELETE":
+#         article.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
